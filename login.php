170d6ff1a72821543b66290f1d0f1f617a9df3ca
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2021-12-17 20:32:05 +0700

    update kkkk

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+    if(isset($_POST['user'])&&isset($_POST['pass'])){
+        require_once "conn.php";
+        require_once "validate.php";
+        $user = validate($_POST['user']);
+        $pass = validate($_POST['pass']);
+        $arrayuser = array();
+        $sql = "select nguoiDung.*, phongban.tenPhongBan , quyen.idQuyen from phongban, nguoidung , quyen, phanquyen where taiKhoan='$user' and matkhau='$pass' and phongban.idPhongBan = nguoidung.idPhongBan and nguoidung.idPhongBan = phanquyen.idPhongBan and phanquyen.idQuyen = quyen.idQuyen";
+	$result = mysqli_query($connect,$sql);
+        while($row = mysqli_fetch_assoc($result)){
+          array_push($arrayuser, new user(
+                $row['taiKhoan'],
+                $row['hoten'],
+                $row['matkhau'],
+                $row['idPhongBan'],
+                $row['tenPhongBan'],
+                $row['ngaysinh'],
+                $row['diachi'],
+                $row['ngaysinh'],
+                $row['gioitinh']
+          ));
+        
+        }
+    }
+    
+    if(count($arrayuser)>0){
+        echo json_encode($arrayuser);
+    }
+    else{
+        echo "failure";
+    }
+        
+
+    class user{
+        function __construct($taiKhoan,$hoten,$matkhau,$idPhongBan,$tenPhongBan,$ngaysinh,$gioitinh,$diachi) {
+            $this->taiKhoan = $taiKhoan;
+            $this->hoten = $hoten;
+            $this->matkhau = $matkhau;
+            $this->idPhongBan = $idPhongBan;  
+            $this->tenPhongBan = $tenPhongBan;  
+            $this->ngaysinh = $ngaysinh;  
+            $this->diachi = $diachi;   
+            $this->ngaysinh = $ngaysinh;  
+            $this->gioitinh = $gioitinh;
+        }
+    }
+   
+?>
