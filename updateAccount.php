824cb5924911995e8a03dc832d70f27c87814645
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2021-12-25 23:11:38 +0700

    Thanh Nhan

--- updateAccount.php
+++ updateAccount.php
@@ -16,11 +16,32 @@
                                      where taiKhoan = '$taiKhoan' ";
 
     $result = mysqli_query($connect, $querry);
-    if($result)
-    {
-        echo "Success";
 
+    $arrayuser = array();
+        $sql = "select nguoiDung.*, phongban.tenPhongBan , quyen.idQuyen from phongban, nguoidung , quyen, phanquyen where taiKhoan = '$taiKhoan' and phongban.idPhongBan = nguoidung.idPhongBan and nguoidung.idPhongBan = phanquyen.idPhongBan and phanquyen.idQuyen = quyen.idQuyen";
+    $result1 = mysqli_query($connect,$sql);
+        while($row = mysqli_fetch_assoc($result1)){
+          array_push($arrayuser, new user(
+                $row['idNguoiDung'],
+                $row['idQuyen'],
+                $row['taiKhoan'],
+                $row['hoten'],
+                $row['matkhau'],
+                $row['idPhongBan'],
+                $row['tenPhongBan'],
+                $row['ngaysinh'],
+                $row['gioitinh'],               
+                $row['diachi']
+          ));
+        
+        }
     }
-    else
-        echo $querry;
+    
+    if(count($arrayuser)>0){
+        echo json_encode($arrayuser);
+    }
+    else{
+        echo "failure";
+    }       
+
 ?>
